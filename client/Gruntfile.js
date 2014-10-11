7e2f3a94c2a71cf0c7b08b7b63303b9f1daf8010
Jason Swett <jason@benfranklinlabs.com>
2014-10-11 11:04:43 -0400

    Remove public directory.

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: 'dist'
+    dist: '../public'
   };
 
   var modRewrite = require('connect-modrewrite');
