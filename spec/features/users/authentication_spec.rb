3703af58804c06bca1fd69ef6991bd151bd83c6a
Jason Swett <jason@benfranklinlabs.com>
2014-10-13 13:43:10 -0700

    Add login failure message.

--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -5,15 +5,21 @@ feature 'Authentication', js: true do
 		@user = FactoryGirl.create(:confirmed_user)
     visit '/sign_in'
     @login_page = LoginPage.new
-    @login_page.sign_in(@user.email, @user.password)
   end
 
   feature 'login' do
     scenario 'with valid inputs' do
+      @login_page.sign_in(@user.email, @user.password)
       expect(page).to have_content('Sign out')
     end
 
+    scenario 'with invalid credentials' do
+      @login_page.sign_in('invalid@lol.com', 'not the actual password')
+      expect(page).to have_content('Invalid login credentials. Please try again.')
+    end
+
     scenario 'redirect after login' do
+      @login_page.sign_in(@user.email, @user.password)
       expect(page).to have_content('This is the home page.')
     end
   end
